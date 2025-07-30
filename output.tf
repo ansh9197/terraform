522f2b440a4890cff2a906e4d6e3e0a03fe59ab9
Harshit Arora <ansh55028@gmail.com>
2025-07-30 12:04:39 +0530

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "ec2_public_ip" {
+    value = aws_instance.my_instance.public_ip  
+}
+output "public_dns" {
+    value = aws_instance.my_instance.public_dns
+  
+}
+output "private_ip" {
+    value = aws_instance.my_instance.private_ip
+  
+}
